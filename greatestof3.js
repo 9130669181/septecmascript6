d4cd02a8aa81ad45d36ae250a68cf692e85010e1
shafiyoddin <shafi9960@gmail.com>
2021-09-20 18:01:22 +0530

    Update greatestof3.js
    best regards

--- greatestof3.js
+++ greatestof3.js
@@ -1,7 +1,15 @@
 const greaterOf3=(num1,num2,num3)=>{
     if((num1>num2)&&(num1>num3)){
         return num1
-    }else{
-if((num2>num1)&&(num2>num3))
-    }
+    }else if
+((num2>num1)&&(num2>num3)){
+    return num2;
+} else{
+    return num3;
+
 }
+}
+console.log(greaterOf3(5,3,7))
+
+
+
